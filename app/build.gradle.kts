1d4c4b29dea46fac7c8ea33328ba2f5ff2621530
george <george.moore@cyted.ai>
2025-08-23 13:40:11 +0100

    add serialization plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 
 plugins {
     id("org.jetbrains.kotlin.jvm") version "2.1.10"
+    kotlin("plugin.serialization") version "2.1.10"
     id("org.openapi.generator") version "7.0.0"
     application
 }
